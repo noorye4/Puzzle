dcbf8587e1c52fdf9f3ccb49c9be4fe1d71278d5
noorye4 <noorye4@gmail.com>
2014-08-11 18:50:23 +0800

    commit

--- puzzle.py
+++ puzzle.py
@@ -47,40 +47,30 @@ def piece_wrap(split_list):
 
 def calc_pixle_dis(src_piece_list,edge_depth = 10):
 
-
     for src_piece in src_piece_list:
-
         #pixel calc
-        src_cv2_img = src_piece.piece_content
-        src_piece_h = src_cv2_img.shape[0]
-        src_piece_w = src_cv2_img.shape[1]
-
-        #for w in range(src_piece_w):
-            #for h in range(edge_depth):
-                #src_cv2_img[h,w] = [255,255,255]
-                #src_cv2_img[(src_piece_w-1)-h,w] = [255,255,255]
-                #src_cv2_img[w,h] = [255,255,255]
-                #src_cv2_img[w,(src_piece_h-1)-h] = [255,255,255]
-        print src_piece.piece_id
-        print "#"*10
-        cv2.imshow("piece",src_cv2_img)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
-
         for des_piece in src_piece_list:
             if des_piece.piece_id == src_piece.piece_id:
                 pass
             else:
 
+                src_cv2_img = src_piece.piece_content
                 des_cv2_img = des_piece.piece_content
+
+                src_piece_h = src_cv2_img.shape[0]
+                src_piece_w = src_cv2_img.shape[1]
                 des_piece_h = des_cv2_img.shape[0]
                 des_piece_w = des_cv2_img.shape[1]
 
-                print des_piece.piece_id
-                print "#"*10
-                cv2.imshow("piece",des_cv2_img)
-                cv2.waitKey()
-                cv2.destroyAllWindows()
+                for w in range(src_piece_w):
+                    for h in range(edge_depth):
+                        src_cv2_img[h,w] = [255,255,255]
+                        src_cv2_img[(src_piece_w-1)-h,w] = [255,255,255]
+                        src_cv2_img[w,h] = [255,255,255]
+                        src_cv2_img[w,(src_piece_h-1)-h] = [255,255,255]
+                #cv2.imshow("piece",des_cv2_img)
+                #cv2.waitKey()
+                #cv2.destroyAllWindows()
 
 #sqrt( (Xr-Yr)^2 + (Xg-Yg)^2 + (Xb-Yb)^2 )
 #left side
